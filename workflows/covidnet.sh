9390e5d5dee02ea50f0038695310106389ade13a
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2021-01-25 20:48:47 -0500

    Update inline help, update for use with new ffe.sh

--- workflows/covidnet.sh
+++ workflows/covidnet.sh
@@ -13,35 +13,31 @@ source ./ffe.sh
 # to templatize/describe feedflow structure.
 #
 declare -a a_WORKFLOWSPEC=(
-    "
-     # Define a 'loop', a variable, and an iterator expression
-     # The variable can be expanded over the iterator range
-     # to create a full feed/branch structure.
-     # NOTE: Obviously still WIP
-    "
-    "l1:_n:seq 1 5"
 
     "0:0|
-    fnndsc/pl-lungct:           --NOARGS"
+    fnndsc/pl-lungct:           NOARGS"
 
     "0:1*_n:l1|
-    fnndsc/pl-med2img:          --inputFile=@image[_n];
+    fnndsc/pl-med2img:          ARGS;
+                                --inputFile=@image[_n];
                                 --convertOnlySingleDICOM;
                                 --previous_id=@prev_id"
 
     "1*_n:2*_n:l1|
-    fnndsc/pl-covidnet:         --imagefile=sample.png;
+    fnndsc/pl-covidnet:         ARGS;
+                                --imagefile=sample.png;
                                 --previous_id=@prev_id"
 
     "2*_n:3*_n:l1|
-    fnndsc/pl-pdfgeneration:    --imagefile=sample.png;
+    fnndsc/pl-pdfgeneration:    ARGS;
+                                --imagefile=sample.png;
                                 --patientId=@patientID;
                                 --previous_id=@prev_id"
 )
 
 WORKFLOW=\
 '{
-
+    "WARNING":  "THIS JSON STRUCTURE IS NOT USED!!!"
     "meta": {
         "loops":    [
             {
@@ -98,8 +94,7 @@ declare -a a_PLUGINS=()
 declare -a a_ARGS=()
 pluginArray_filterFromWorkflow  "a_WORKFLOWSPEC[@]" "a_PLUGINS"
 argArray_filterFromWorkflow     "a_WORKFLOWSPEC[@]" "a_ARGS"
-a_PLUGINS=($a_PLUGINS)
-a_ARGS=($a_ARGS)
+
 # ||||||||||||||||||||||||||||||||||
 # end feedflow specification section
 # //////////////////////////////////
@@ -121,7 +116,9 @@ SYNPOSIS
                         [-w <passwd>]                       \\
                         [-G <graphvizDotFile>]              \\
                         [-i <listOfLungImagesToProcess>]    \\
+                        [-s <sleepAfterPluginRun>]          \\
                         [-W]                                \\
+                        [-S]                                \\
                         [-R]                                \\
                         [-J]                                \\
                         [-q]
@@ -159,6 +156,16 @@ DESC
 
 ARGS
 
+    [-s <sleepAfterPluginRun>]
+    Default is '0'. Adds an explicit system ``sleep`` after executing
+    a plugin. This can be useful in not overloading the ancillary
+    services when large amount of plugins are being dispatched
+    concurrently.
+
+    [-S]
+    If specified, save each plugin POST command on the filesystem. Useful
+    for debugging.
+
     [-W]
     If specified, will wait at the end of a single branch for success
     of termination node before building a subsequent branch. This
@@ -176,10 +183,21 @@ ARGS
     pl-covidnet. Note that this implies a [-W].
 
     [-G <graphvizDotFile>]
-    If specified, write a graphviz .dot file that describes the workflow
-    and is suitable for rendering by graphviz parsers, e.g.
+    If specified, write two graphviz .dot files called
+
+                        <graphvizDotFile>-nodes.dot
+                     <graphvizDotFile>-nodes-args.dot
+
+    that describes the workflow in graphviz format. The first dot file
+    contains only the nodes in the tree, while the second contains the nodes
+    with graph edges labeled with the CLI args denoting the tranition from
+    one node to another.
+
+    These dot files are suitable for rendering by graphviz parsers, e.g.
 
                 http://dreampuf.github.io/GraphvizOnline
+                http://viz-js.com
+
 
     [-i <listOflLungImageToProcess>]
     Runs the inference pipeline of each of the comma separated images
@@ -226,17 +244,45 @@ EXAMPLES
 
     Typical execution:
 
-    $ ./covidnet.sh  -C '{
-               \"protocol\":     \"http\",
-               \"port\":         \"8000\",
-               \"address\":      \"megalodon.local\",
-               \"user\":         \"chris\",
-               \"password\":     \"chris1234\"
-    }'
+        $ ./covidnet.sh  -C '{
+                   \"protocol\":     \"http\",
+                   \"port\":         \"8000\",
+                   \"address\":      \"megalodon.local\",
+                   \"user\":         \"chris\",
+                   \"password\":     \"chris1234\"
+        }'
 
     or equivalently:
 
-    $ ./covidnet.sh -a megalodon.local
+        $ ./covidnet.sh -a megalodon.local
+
+TIMING CONSIDERATIONS
+
+    While this client script should ideally not concern itself with execution
+    concerns beyond the logical structure of a feedflow, some notes are
+    important:
+
+        * Too many apps POSTed in quick succession *might* overwhelm the
+          scheduler;
+
+    To not overwhelm the scheduler, it is a good idea to pause for a few
+    seconds after POSTing each app to the backend with a '-s 3' (for 3s
+    pause) flag.
+
+    Thus,
+
+        $ ./covidnet.sh -a megalodon.local -s 3 -G feed
+
+    where the '-G feed' also produces two graphviz dot files suitable for
+    rendering with a graphviz viewer.
+
+REPORTING CONSIDERATIONS
+
+    This script can also fetch final prediction data and write a summary
+    report to the console. This is set using a '-R' (report) flag. Note that
+    to generate the report forces execution to run essentially in series and
+    not in parallel. This is so as to print the correct report with the correct
+    compute branch.
 
 "
 
@@ -255,18 +301,18 @@ CUBE_FMT='{
         "password":     "%s"
 }'
 
-GRAPHVIZ="
-digraph G {
+GRAPHVIZHEADER='digraph G {
+    rankdir="LR";
 
-  subgraph cluster_0 {
-    style=filled;
-    color=lightgrey;
-    node [style=filled,color=white,fontname='mono',fontsize=8];
-    %s
-    label = 'ChRIS COVIDNET Feedgraph';
-  }
-}
-"
+    subgraph cluster_0 {
+        style=filled;
+        color=lightgrey;
+        label = "ChRIS COVID-NET Graph";
+        node [style=filled,fillcolor=white,fontname="mono",fontsize=8];
+        edge [fontname="mono", fontsize=8];
+'
+GRAPHVIZBODY=""
+GRAPHVIZBODYARGS=""
 
 declare -i b_respSuccess=0
 declare -i b_respFail=0
@@ -278,11 +324,15 @@ declare -i b_graphviz=0
 declare -i b_waitOnBranchFinish=0
 declare -i b_printReport=0
 declare -i b_printJSONprediction=0
+declare -i sleepAfterPluginRun=0
+declare -i b_saveCalls=0
 IMAGESTOPROCESS=""
 GRAPHVIZFILE=""
 
-while getopts "C:G:i:qxr:p:a:u:w:WRJ" opt; do
+while getopts "C:G:i:qxr:p:a:u:w:WRJs:S" opt; do
     case $opt in
+        S) b_saveCalls=1                        ;;
+        s) sleepAfterPluginRun=$OPTARG          ;;
         W) b_waitOnBranchFinish=1               ;;
         R) b_waitOnBranchFinish=1
            b_printReport=1                      ;;
@@ -335,7 +385,7 @@ title -d 1 "Checking for plugin IDs on CUBE...."                            \
     #
     b_respSuccess=0
     b_respFail=0
-    boxcenter "Verify that all the plugin that constitute this workflow are    "
+    boxcenter "Verify that all the plugins that constitute this workflow are    "
     boxcenter "registered to the CUBE instance with which we are communicating."
     boxcenter ""
     for plugin in "${a_PLUGINS[@]}" ; do
@@ -373,7 +423,8 @@ title -d 1 "Start constructing the Feed by POSTing the root FS node..."
 
     #\\\\\\\\\\\\\\\\\\
     # Core logic here ||
-    plugin_run          "0:0"   "a_WORKFLOWSPEC[@]"   "$CUBE"  ROOTID
+    plugin_run          "0:0"   "a_WORKFLOWSPEC[@]"   "$CUBE"  ROOTID \
+                        $sleepAfterPluginRun && id_check $ROOTID
     waitForNodeState    "$CUBE" "finishedSuccessfully" $ROOTID retState
     dataInNode_get      fname "$CUBE"  $ROOTID filesInNode
     # Core logic here ||
@@ -430,6 +481,8 @@ title -d 1 "Building and Scheduling workflow..."
     boxcenter ""
     boxcenter "If a report has been specified, print a final report on the "
     boxcenter "prediction of the input image for that branch.              "
+    boxcenter ""
+
     # Now the branch(es)
     b_respSuccess=1
     b_respFail=0
@@ -442,12 +495,20 @@ title -d 1 "Building and Scheduling workflow..."
         boxcenter ""
         boxcenter ""
 
-        plugin_run  ":1" "a_WORKFLOWSPEC[@]" "$CUBE" ID1 \
-                    "@prev_id=$ROOTID;@image[_n]=$image"
-        plugin_run  ":2" "a_WORKFLOWSPEC[@]" "$CUBE" ID2 \
-                    "@prev_id=$ID1"
-        plugin_run  ":3" "a_WORKFLOWSPEC[@]" "$CUBE" ID3 \
-                    "@prev_id=$ID2;@patientID=$ID1-12345"
+        plugin_run  ":1" "a_WORKFLOWSPEC[@]" "$CUBE" ID1 $sleepAfterPluginRun \
+                    "@prev_id=$ROOTID;@image[_n]=$image" && id_check $ID1
+        digraph_add "GRAPHVIZBODY"  "GRAPHVIZBODYARGS" ":0;$ROOTID" ":1;$ID1" \
+                    "a_WORKFLOWSPEC[@]"
+
+        plugin_run  ":2" "a_WORKFLOWSPEC[@]" "$CUBE" ID2 $sleepAfterPluginRun \
+                    "@prev_id=$ID1" && id_check $ID2
+        digraph_add "GRAPHVIZBODY" "GRAPHVIZBODYARGS" ":1;$ID1" ":2;$ID2" \
+                    "a_WORKFLOWSPEC[@]"
+
+        plugin_run  ":3" "a_WORKFLOWSPEC[@]" "$CUBE" ID3 $sleepAfterPluginRun \
+                    "@prev_id=$ID2;@patientID=$ID1-12345" && id_check $ID3
+        digraph_add "GRAPHVIZBODY" "GRAPHVIZBODYARGS" ":2;$ID2" ":3;$ID3" \
+                    "a_WORKFLOWSPEC[@]"
 
         if (( b_waitOnBranchFinish )) ; then
             waitForNodeState    "$CUBE" "finishedSuccessfully" $ID3 retState
@@ -501,3 +562,10 @@ title -d 1 "Building and Scheduling workflow..."
     postRun_report
 windowBottom
 if (( b_respFail > 0 )) ; then exit 3 ; fi
+
+if (( b_graphviz )) ; then
+    graphVis_printFile "$GRAPHVIZHEADER"    \
+                        "$GRAPHVIZBODY"     \
+                        "$GRAPHVIZBODYARGS" \
+                        "$GRAPHVIZFILE"
+fi
