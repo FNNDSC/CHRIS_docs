5f02206eb7fc82d401d043f2d3a26583af98fcf7
Sandip Samal <32147904+Sandip117@users.noreply.github.com>
2021-02-06 15:36:46 -0500

    Delete pl-mgz2imageslices-3be58d0a-78af-4e5c-8224-11417c8b2f74.sh

--- workflows/pl-mgz2imageslices-3be58d0a-78af-4e5c-8224-11417c8b2f74.sh
+++
@@ -1,4 +0,0 @@
-chrispl-run \
-		--plugin   name=pl-mgz2imageslices \
-		--args     "ARGS;--inputFile=subjects/100408/brain.mgz;--outputFileStem=sample;--outputFileType=png;--lookupTable=__none__;--skipAllLabels;--saveImages;--wholeVolume=entireVolume;--verbosity=5;--previous_id=104"  \
-		--onCUBE   '{"protocol":"http","port":"8000","address":"117.local","user":"chris","password":"chris1234"}'
